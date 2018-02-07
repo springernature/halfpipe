232f1cbedcf4be5358a0f5df808feb9b93617db0
Simon Johansson <simon@simonjohansson.com>
2018-02-07 15:52:12 +0100

    Bake in version in binary from concourse

--- cmd/halfpipe.go
+++ cmd/halfpipe.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
+
+var(
+	// This field will be populated in Concourse from the version resource
+	// go build -ldflags "-X main.version`cat version/version`"
+	version string
+)
+
+func getVersion() string {
+	if version == "" {
+		return "dev"
+	}
+	return version
+}
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("Current version is: " + getVersion())
 }
