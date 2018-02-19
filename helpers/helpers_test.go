14543da63fce2f8ef29a206a2ef615012b14e825
Holger Protzek <holger.protzek@springer.com>
2018-02-19 10:37:08 +0100

    added helper test

---
+++ helpers/helpers_test.go
@@ -0,0 +1,14 @@
+package helpers
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSecretsToMapSuccessfull(t *testing.T) {
+	secretStr := "((foo.key))"
+	name, key := SecretToMapAndKey(secretStr)
+
+	assert.Equal(t, "foo", name)
+	assert.Equal(t, "key", key)
+}
