c856aded458d020e559ce04e661481498a5536e9
Rob Whitby <rob.whitby@springer.com>
2018-03-09 19:04:40 +0000

    add golint to build

--- build.sh
+++ build.sh
@@ -17,8 +17,20 @@ if ! ds=$(dep status 2> /dev/null); then
     exit 1
 fi
 
+#lint but ignore some of the more contentious warnings ;)
+echo golint
+if command -v golint > /dev/null; then
+    golint `go list ./... | grep -v /vendor/` |
+        grep -v 'should have comment' |
+        grep -v 'comment on exported' |
+        grep -v 'returns unexported type'
+else
+    echo "skipping. to install: go get -u golang.org/x/lint/golint"
+fi
+
 echo go build
 LD_VAULTPREFIX="-X github.com/springernature/halfpipe/config.VaultPrefix=springernature"
 LD_DOCHOST="-X github.com/springernature/halfpipe/config.DocHost=docs.halfpipe.io"
 LD_SLACKWEBHOOK="-X github.com/springernature/halfpipe/config.SlackWebhook=https://hooks.slack.com/services/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"
 go build -ldflags "${LD_VAULTPREFIX} ${LD_DOCHOST} ${LD_SLACKWEBHOOK}" cmd/halfpipe.go
+
