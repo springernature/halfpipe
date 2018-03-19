468a5f7a27f7518e482e843de722b108fba6e43d
Rob Whitby <rob.whitby@springer.com>
2018-03-19 13:32:28 +0000

    improve bash helper funcs

--- extras/halfpipe-helpers.sh
+++ extras/halfpipe-helpers.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env bash
 
+# put something like this in your ~/.bash_profile
+# [ -f ~/go/src/github.com/springernature/halfpipe/extras/halfpipe-helpers.sh ] && source ~/go/src/github.com/springernature/halfpipe/extras/halfpipe-helpers.sh
+
+# assumes concourse.halfpipe.io is setup as target "hp". don't argue.
+readonly target=hp
+
 # Update pipeline.yml and push to Concourse
 # $1 pipeline-name (optional - defaults to current directory name)
 hp() {
   echo Updating pipeline.yml
-  halfpipe > pipeline.yml
-  echo Uploading to Concourse
-  fly-set $1
+  halfpipe > pipeline.yml &&
+    echo Uploading to Concourse &&
+      hp-set $1
 }
 
 # Login to Concourse
 # $1 team-name (optional - defaults to engineering-enablement)
-fly-login() {
+hp-login() {
   team=${1:-"engineering-enablement"}
-  fly -t hp login -n $team
+  fly -t $target login -n $team
 }
 
 # Get a pipeline
 # $1 pipeline-name (optional - defaults to current directory name)
-fly-get() {
+hp-get() {
   dir=$(basename "$PWD")
   pipeline=${1:-$dir}
-  fly -t hp get-pipeline -p $pipeline
+  fly -t $target get-pipeline -p $pipeline
 }
 
 # Set a pipeline
 # $1 pipeline-name (optional - defaults to current directory name)
 # $2 config-yaml   (optional - defaults to pipeline.yml)
-fly-set() {
+hp-set() {
   dir=$(basename "$PWD")
   pipeline=${1:-$dir}
   config=${2:-pipeline.yml}
-  setp="fly -t hp set-pipeline -p $pipeline -c $config"
+  setp="fly -t $target set-pipeline -p $pipeline -c $config"
   echo ">> $setp"
   $setp
 }
