c984882d563cd9ad5cf751022d978dc9ae9af084
StigterMichiel <michiel.stigter@springer.com>
2018-06-13 16:05:47 +0100

    refactoring duplication of switch statements

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -45,92 +45,61 @@ func (p pipeline) addOnFailurePlan(cfg *atc.Config, man manifest.Manifest) *atc.
 	onFailurePlanSet := man.OnFailure != nil
 
 	if slackChannelSet && onFailurePlanSet {
-		var planSequence atc.PlanSequence
-		slackResource := p.slackResource()
-		cfg.Resources = append(cfg.Resources, slackResource)
-
-		slackResourceType := p.slackResourceType()
-		cfg.ResourceTypes = append(cfg.ResourceTypes, slackResourceType)
-		slackPlanConfig := atc.PlanConfig{
-			Put: slackResource.Name,
-			Params: atc.Params{
-				"channel":  man.SlackChannel,
-				"username": "Halfpipe",
-				"icon_url": "https://concourse.halfpipe.io/public/images/favicon-failed.png",
-				"text":     "The pipeline `$BUILD_PIPELINE_NAME` failed at `$BUILD_JOB_NAME`. <$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME>",
-			},
-		}
-
-		planSequence = append(planSequence, slackPlanConfig)
-
-		for _, t := range man.OnFailure {
-			var onFailureJob *atc.JobConfig
-			switch ppTask := t.(type) {
-			case manifest.Run:
-				if len(ppTask.Vars) == 0 {
-					ppTask.Vars = make(map[string]string)
-				}
-				ppTask.Name = uniqueName(cfg, ppTask.Name, fmt.Sprintf("run %s", strings.Replace(ppTask.Script, "./", "", 1)))
-				onFailureJob = p.runJob(ppTask, true, man)
-			case manifest.DockerCompose:
-				if len(ppTask.Vars) == 0 {
-					ppTask.Vars = make(map[string]string)
-				}
-				ppTask.Name = uniqueName(cfg, ppTask.Name, "docker-compose")
-				onFailureJob = p.dockerComposeJob(ppTask, man)
-			}
-			planSequence = append(planSequence, onFailureJob.Plan...)
-		}
-
-		return &atc.PlanConfig{
-			Do: &planSequence,
-		}
+		slackPlan := p.addSlackPlanConfig(cfg, man)
+		planSequence := atc.PlanSequence{*slackPlan}
+		planSequence = append(planSequence, p.addOnFailureJob(cfg, man)...)
+		return &atc.PlanConfig{Do: &planSequence}
 	}
 
 	if slackChannelSet {
-		slackResource := p.slackResource()
-		cfg.Resources = append(cfg.Resources, slackResource)
-
-		slackResourceType := p.slackResourceType()
-		cfg.ResourceTypes = append(cfg.ResourceTypes, slackResourceType)
-
-		return &atc.PlanConfig{
-			Put: slackResource.Name,
-			Params: atc.Params{
-				"channel":  man.SlackChannel,
-				"username": "Halfpipe",
-				"icon_url": "https://concourse.halfpipe.io/public/images/favicon-failed.png",
-				"text":     "The pipeline `$BUILD_PIPELINE_NAME` failed at `$BUILD_JOB_NAME`. <$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME>",
-			},
-		}
+		return p.addSlackPlanConfig(cfg, man)
 	}
 
 	if onFailurePlanSet {
-		var planSequence atc.PlanSequence
-		for _, t := range man.OnFailure {
-			var onFailureJob *atc.JobConfig
-			switch ppTask := t.(type) {
-			case manifest.Run:
-				if len(ppTask.Vars) == 0 {
-					ppTask.Vars = make(map[string]string)
-				}
-				ppTask.Name = uniqueName(cfg, ppTask.Name, fmt.Sprintf("run %s", strings.Replace(ppTask.Script, "./", "", 1)))
-				onFailureJob = p.runJob(ppTask, true, man)
-			case manifest.DockerCompose:
-				if len(ppTask.Vars) == 0 {
-					ppTask.Vars = make(map[string]string)
-				}
-				ppTask.Name = uniqueName(cfg, ppTask.Name, "docker-compose")
-				onFailureJob = p.dockerComposeJob(ppTask, man)
-			}
-			planSequence = append(planSequence, onFailureJob.Plan...)
-		}
-		return &atc.PlanConfig{
-			Do: &planSequence,
-		}
+		planSequence := p.addOnFailureJob(cfg, man)
+		return &atc.PlanConfig{Do: &planSequence}
 	}
 
 	return nil
+}
+
+func (p pipeline) addOnFailureJob(cfg *atc.Config, man manifest.Manifest) (planSequence atc.PlanSequence) {
+	for _, t := range man.OnFailure {
+		var onFailureJob *atc.JobConfig
+		switch ppTask := t.(type) {
+		case manifest.Run:
+			if len(ppTask.Vars) == 0 {
+				ppTask.Vars = make(map[string]string)
+			}
+			ppTask.Name = uniqueName(cfg, ppTask.Name, fmt.Sprintf("run %s", strings.Replace(ppTask.Script, "./", "", 1)))
+			onFailureJob = p.runJob(ppTask, true, man)
+		case manifest.DockerCompose:
+			if len(ppTask.Vars) == 0 {
+				ppTask.Vars = make(map[string]string)
+			}
+			ppTask.Name = uniqueName(cfg, ppTask.Name, "docker-compose")
+			onFailureJob = p.dockerComposeJob(ppTask, man)
+		}
+		planSequence = append(planSequence, onFailureJob.Plan...)
+	}
+	return planSequence
+}
+
+func (p pipeline) addSlackPlanConfig(cfg *atc.Config, man manifest.Manifest) *atc.PlanConfig {
+	slackResource := p.slackResource()
+	cfg.Resources = append(cfg.Resources, slackResource)
+	slackResourceType := p.slackResourceType()
+	cfg.ResourceTypes = append(cfg.ResourceTypes, slackResourceType)
+	slackPlanConfig := atc.PlanConfig{
+		Put: slackResource.Name,
+		Params: atc.Params{
+			"channel":  man.SlackChannel,
+			"username": "Halfpipe",
+			"icon_url": "https://concourse.halfpipe.io/public/images/favicon-failed.png",
+			"text":     "The pipeline `$BUILD_PIPELINE_NAME` failed at `$BUILD_JOB_NAME`. <$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME>",
+		},
+	}
+	return &slackPlanConfig
 }
 
 func (p pipeline) initialPlan(cfg *atc.Config, man manifest.Manifest) []atc.PlanConfig {
