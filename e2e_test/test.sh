ccf9d4c517e804adeee0038f30ecab96b22058ac
Rob Whitby <rob.whitby@springer.com>
2018-08-25 23:55:29 +0100

    don't require fly for e2e test

--- e2e_test/test.sh
+++ e2e_test/test.sh
@@ -5,6 +5,6 @@ HALFPIPE_PATH=${1:-"../halfpipe"}
 
 ${HALFPIPE_PATH} | tee pipeline.yml
 
-fly validate-pipeline -c pipeline.yml
+command -v fly >/dev/null && fly validate-pipeline -c pipeline.yml
 
 diff -w pipeline.yml expected-pipeline.yml
