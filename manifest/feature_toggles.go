fc70e200bd3cd2b1ec631aa975631ea3f125985c
Rob Whitby <rob.whitby@springer.com>
2019-01-10 11:43:04 +0000

    add feature toggle 'auto-update' which is just an alias for 'versioned'

--- manifest/feature_toggles.go
+++ manifest/feature_toggles.go
@@ -3,16 +3,26 @@ package manifest
 type FeatureToggles []string
 
 const FeatureVersioned = "versioned"
+const FeatureAutoUpdate = "auto-update"
 
 var AvailableFeatureToggles = FeatureToggles{
 	FeatureVersioned,
+	FeatureAutoUpdate,
 }
 
-func (f FeatureToggles) Versioned() bool {
+func (f FeatureToggles) contains(aFeature string) bool {
 	for _, feature := range f {
-		if feature == FeatureVersioned {
+		if feature == aFeature {
 			return true
 		}
 	}
 	return false
+}
+
+func (f FeatureToggles) Versioned() bool {
+	return f.contains(FeatureVersioned) || f.contains(FeatureAutoUpdate)
+}
+
+func (f FeatureToggles) AutoUpdate() bool {
+	return f.contains(FeatureAutoUpdate)
 }
