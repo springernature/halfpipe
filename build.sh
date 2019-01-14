f4083e791d99b4e05ad245f941a83b7ebcd507f2
Rob Whitby <rob.whitby@springer.com>
2019-01-14 16:03:53 +0000

    do lint after everything else cos it's a bit slow

--- build.sh
+++ build.sh
@@ -19,7 +19,16 @@ go fmt ./...
 echo [2/5] test
 go test $go_opts -cover ./...
 
-echo [3/5] lint
+echo [3/5] build
+CONF_PKG="github.com/springernature/halfpipe/config"
+LDFLAGS="-X ${CONF_PKG}.DocHost=docs.halfpipe.io"
+LDFLAGS="${LDFLAGS} -X ${CONF_PKG}.SlackWebhook=https://hooks.slack.com/services/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"
+go build $go_opts -ldflags "${LDFLAGS}" cmd/halfpipe.go
+
+echo [4/5] e2e test
+cd e2e; ./test.sh "${1-}"; cd - > /dev/null
+
+echo [5/5] lint
 if command -v golint > /dev/null; then
     golint ./... |
         grep -v 'should have comment or be unexported' |
@@ -28,14 +37,5 @@ if command -v golint > /dev/null; then
 else
     echo "golint not installed. to install: go get -u golang.org/x/lint/golint"
 fi
-
-echo [4/5] build
-CONF_PKG="github.com/springernature/halfpipe/config"
-LDFLAGS="-X ${CONF_PKG}.DocHost=docs.halfpipe.io"
-LDFLAGS="${LDFLAGS} -X ${CONF_PKG}.SlackWebhook=https://hooks.slack.com/services/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"
-go build $go_opts -ldflags "${LDFLAGS}" cmd/halfpipe.go
-
-echo [5/5] e2e test
-cd e2e; ./test.sh "${1-}"
 
 echo Finished!
