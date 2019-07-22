e1d2038a4cb452757523fab4052c11c4b88248a4
Rob Whitby <rob.whitby@springer.com>
2019-07-22 15:01:34 +0100

    refactor slack success and failure plans into one

--- pipeline/noitification.go
+++ pipeline/noitification.go
@@ -1,27 +1,27 @@
 package pipeline
 
-import "github.com/concourse/concourse/atc"
+import (
+	"fmt"
+	"github.com/concourse/concourse/atc"
+)
 
 func slackOnFailurePlan(channel string) atc.PlanConfig {
-	return atc.PlanConfig{
-		Put: slackResourceName,
-		Params: atc.Params{
-			"channel":  channel,
-			"username": "Halfpipe",
-			"icon_url": "https://concourse.halfpipe.io/public/images/favicon-failed.png",
-			"text":     "The pipeline `$BUILD_PIPELINE_NAME` failed at `$BUILD_JOB_NAME`. <$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME>",
-		},
-	}
+	return slackPlan(channel, "failed")
 }
 
 func slackOnSuccessPlan(channel string) atc.PlanConfig {
+	return slackPlan(channel, "succeeded")
+}
+
+func slackPlan(channel string, status string) atc.PlanConfig {
+	url := "<$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME>"
 	return atc.PlanConfig{
 		Put: slackResourceName,
 		Params: atc.Params{
 			"channel":  channel,
 			"username": "Halfpipe",
-			"icon_url": "https://concourse.halfpipe.io/public/images/favicon-succeeded.png",
-			"text":     "Pipeline `$BUILD_PIPELINE_NAME`, Task `$BUILD_JOB_NAME` succeeded",
+			"icon_url": fmt.Sprintf("https://concourse.halfpipe.io/public/images/favicon-%s.png", status),
+			"text":     fmt.Sprintf("Pipeline `$BUILD_PIPELINE_NAME`, task `$BUILD_JOB_NAME` %s %s", status, url),
 		},
 	}
 }
