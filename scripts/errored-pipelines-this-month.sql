9f077e8c1e59b9e35d53ed2f38c3079f1163042a
Simon Johansson <simon@simonjohansson.com>
2019-10-14 11:37:28 +0200

    Update errored-pipelines-this-month.sql

--- scripts/errored-pipelines-this-month.sql
+++ scripts/errored-pipelines-this-month.sql
@@ -1,11 +1,28 @@
+create or replace function urlencode(in_str text, OUT _result text) returns text as $$
+  select
+    string_agg(
+      case
+        when ol>1 or ch !~ '[0-9a-za-z:/@._?#-]+'
+          then regexp_replace(upper(substring(ch::bytea::text, 3)), '(..)', E'%\\1', 'g')
+        else ch
+      end,
+      ''
+    )
+  from (
+    select ch, octet_length(ch) as ol
+    from regexp_split_to_table($1, '') as ch
+  ) as s;
+$$ language sql immutable strict;
+
 select
+	b.id,
 	concat (
 		'https://concourse.halfpipe.io/teams/',
 		t.name,
 		'/pipelines/',
 		p.name,
 		'/jobs/',
-		j.name,
+		urlencode(j.name),
 		'/builds/',
 		b.name
 	)
@@ -22,4 +39,6 @@ LEFT JOIN
 	ON t.id = p.team_id
 WHERE
 	b.create_time > date_trunc('month', CURRENT_DATE) AND
-	b.status = 'errored';
+	b.status = 'errored'
+ORDER BY
+	b.id;
