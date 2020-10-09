5aa12220b18f2895ade870abe4996234b46466e9
Simon Johansson <simon@simonjohansson.com>
2020-10-09 13:31:14 +0200

    Move concourse tests into its own folder

--- e2e/test.sh
+++ e2e/test.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
-for dir in */
+for system in */
 do
-    dir=${dir%*/}
-    echo "  * Running ${dir}"
-    (
-        cd ${dir}
-        if [[ -f test.sh ]]; then
-            ./test.sh
-        else
-            ../../halfpipe 1> pipeline.yml
-            diff --ignore-blank-lines pipeline.yml expected-pipeline.yml
-            if command -v fly > /dev/null; then
-                fly validate-pipeline -c pipeline.yml > /dev/null
+  system=${system%*/}
+  echo "* Running ${system}"
+  (
+    cd $system
+    for dir in */
+    do
+        dir=${dir%*/}
+        echo "  * Running ${dir}"
+        (
+            cd ${dir}
+            if [[ -f test.sh ]]; then
+                ./test.sh
+            else
+                ../../../halfpipe 1> pipeline.yml
+                diff --ignore-blank-lines pipeline.yml expected-pipeline.yml
+                if command -v fly > /dev/null; then
+                    fly validate-pipeline -c pipeline.yml > /dev/null
+                fi
             fi
-        fi
-    )
+        )
+    done
+  )
 done
