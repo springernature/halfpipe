a2f4dbf210bc18a749f65ece4c24df8f1adaac14
Rob Whitby <rob.whitby@springer.com>
2020-10-21 17:15:36 +0100

    add attempts and timeout to update job

---
+++ renderers/concourse/update_pipeline.go
@@ -0,0 +1,56 @@
+package concourse
+
+import (
+	"github.com/concourse/concourse/atc"
+	"github.com/springernature/halfpipe/config"
+	"github.com/springernature/halfpipe/manifest"
+	"path"
+)
+
+func (p pipeline) updateJobConfig(task manifest.Update, pipelineName string, basePath string) atc.JobConfig {
+
+	const updateTaskAttempts = 2
+	const updateTaskTimeout = "15m"
+
+	update := &atc.TaskStep{
+		Name: "update",
+		Config: &atc.TaskConfig{
+			Platform: "linux",
+			Params: map[string]string{
+				"CONCOURSE_URL":      "((concourse.url))",
+				"CONCOURSE_PASSWORD": "((concourse.password))",
+				"CONCOURSE_TEAM":     "((concourse.team))",
+				"CONCOURSE_USERNAME": "((concourse.username))",
+				"PIPELINE_NAME":      pipelineName,
+				"HALFPIPE_DOMAIN":    config.Domain,
+				"HALFPIPE_PROJECT":   config.Project,
+			},
+			ImageResource: p.imageResource(manifest.Docker{
+				Image:    config.DockerRegistry + "halfpipe-auto-update",
+				Username: "_json_key",
+				Password: "((halfpipe-gcr.private_key))",
+			}),
+			Run: atc.TaskRunConfig{
+				Path: "update-pipeline",
+				Dir:  path.Join(gitDir, basePath),
+			},
+			Inputs: []atc.TaskInputConfig{
+				{Name: manifest.GitTrigger{}.GetTriggerName()},
+			},
+		},
+	}
+
+	bumpVersion := &atc.PutStep{
+		Name:   versionName,
+		Params: atc.Params{"bump": "minor"},
+	}
+
+	return atc.JobConfig{
+		Name:   task.GetName(),
+		Serial: true,
+		PlanSequence: []atc.Step{
+			stepWithAttemptsAndTimeout(update, updateTaskAttempts, updateTaskTimeout),
+			stepWithAttemptsAndTimeout(bumpVersion, updateTaskAttempts, updateTaskTimeout),
+		},
+	}
+}
