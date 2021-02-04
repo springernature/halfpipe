d7ca7f70e4339dccabaf6b8bb0094b0d1def668c
Rob Whitby <rob.whitby@springer.com>
2021-02-04 15:23:29 +0000

    fix warnings

--- migrate/actions_migrate.go
+++ migrate/actions_migrate.go
@@ -15,11 +15,11 @@ func ActionsMigrationHelper(man manifest.Manifest, response halfpipe.Response) (
 	fmt.Println("1. Read the docs over at https://ee.public.springernature.app/rel-eng/github-actions/overview/")
 
 	fmt.Println("2. Pause the Concourse pipeline")
-	fmt.Println(fmt.Sprintf("\t $ fly -t %s pause-pipeline -p %s", man.Team, man.Pipeline))
+	fmt.Printf("\t $ fly -t %s pause-pipeline -p %s\n", man.Team, man.Pipeline)
 
 	fmt.Println("3. Add the necessary Vault tokens to the Github repo's secrets. If you have a mono repo you only need to do this once")
-	fmt.Println(fmt.Sprintf("\t $ vault read springernature/%s/team-ro-app-role", man.Team))
-	fmt.Println(fmt.Sprintf("\t # Write the secrets as VAULT_ROLE_ID and VAULT_SECRET_ID in https://github.com/springernature/%s/settings/secrets/actions", response.Project.RootName))
+	fmt.Printf("\t $ vault read springernature/%s/team-ro-app-role\n", man.Team)
+	fmt.Printf("\t # Write the secrets as VAULT_ROLE_ID and VAULT_SECRET_ID in https://github.com/springernature/%s/settings/secrets/actions\n", response.Project.RootName)
 
 	pwd, err := os.Getwd()
 	if err != nil {
@@ -32,8 +32,8 @@ func ActionsMigrationHelper(man manifest.Manifest, response halfpipe.Response) (
 
 	fmt.Println("4. Render, commit and push the pipeline")
 	fmt.Println("\t $ halfpipe actions")
-	fmt.Println(fmt.Sprintf("\t $ git add %s", path.Join(relPath, ".github", "workflows", man.PipelineName()+".yml")))
-	fmt.Println(fmt.Sprintf("\t $ git commit -m \"Added workflow for %s\"", man.PipelineName()))
+	fmt.Printf("\t $ git add %s\n", path.Join(relPath, ".github", "workflows", man.PipelineName()+".yml"))
+	fmt.Printf("\t $ git commit -m \"Added workflow for %s\"\n", man.PipelineName())
 	fmt.Println("\t $ git push")
 
 	fmt.Println("5. Watch the pipeline in all its glory https://github.com/springernature/halfpipe-examples/actions?query=workflow%3A" + man.PipelineName())
