0ee84a787a05c507660b8b463cf12660594e3c9c
Rob Whitby <rob.whitby@springer.com>
2021-02-11 17:06:10 +0000

    add 'halfpipe actions' back

---
+++ cmd/cmds/actions.go
@@ -0,0 +1,21 @@
+package cmds
+
+import (
+	"fmt"
+	"github.com/gookit/color"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func init() {
+	rootCmd.AddCommand(actionsCmd)
+}
+
+var actionsCmd = &cobra.Command{
+	Use:   "actions",
+	Short: "**NO LONGER SUPPORTED** Generates a GitHub Actions workflow. Set 'platform: actions' in the halfpipe manifest.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Fprintln(os.Stderr, color.FgRed.Sprint("[ERROR] The 'halfpipe actions' command is no longer supported. Set 'platform: actions' in the halfpipe manifest and run 'halfpipe'."))
+		os.Exit(1)
+	},
+}
