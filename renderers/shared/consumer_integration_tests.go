d548a358d29a3a5879c26ac4e30d083ba6a6c2f3
Celeste Phutela <celeste.phutela@springernature.com>
2021-08-26 14:55:24 +0100

    [RW/CP] Refactor cdc renderer

--- renderers/shared/consumer_integration_tests.go
+++ renderers/shared/consumer_integration_tests.go
@@ -2,11 +2,9 @@ package shared
 
 import (
 	"fmt"
-	"strings"
-
-	"sort"
-
 	"github.com/springernature/halfpipe/manifest"
+	"sort"
+	"strings"
 )
 
 func ConsumerIntegrationTestScript(vars manifest.Vars, cacheDirs []string) string {
@@ -17,47 +15,14 @@ func ConsumerIntegrationTestScript(vars manifest.Vars, cacheDirs []string) strin
 	sort.Strings(envStrings)
 	envOption := strings.Join(envStrings, " ")
 
-	var cacheVolumeFlags []string
-	for _, cacheVolume := range cacheDirs {
-		cacheVolumeFlags = append(cacheVolumeFlags, fmt.Sprintf("-v %s:%s", cacheVolume, cacheVolume))
-	}
+	//var cacheVolumeFlags []string
+	//for _, cacheVolume := range cacheDirs {
+	//	cacheVolumeFlags = append(cacheVolumeFlags, fmt.Sprintf("-v %s:%s", cacheVolume, cacheVolume))
+	//}
+	//
+	//volumeOption := strings.Join(cacheVolumeFlags, " ")
 
-	volumeOption := strings.Join(cacheVolumeFlags, " ")
-
-	return fmt.Sprintf(`# write git key to file
-echo "${CONSUMER_GIT_KEY}" > .gitkey
-chmod 600 .gitkey
-
-set -ex
-
-# get current revision of consumer, revert to HEAD if not found
-REVISION=$(curl -fsSL "${CONSUMER_HOST}/internal/version" | jq -r '.revision' || echo "")
-if [ "${REVISION}" == "" ]; then
-  echo "Error fetching version of consumer from ${CONSUMER_HOST}/internal/version - using HEAD instead."
-  REVISION=HEAD
-fi
-
-# clone consumer into /scratch/consumer. dir may already exist when concourse restarts task
-rm -rf /scratch/consumer
-GIT_SSH_COMMAND="ssh -o StrictHostKeychecking=no -i .gitkey" git clone ${GIT_CLONE_OPTIONS} ${CONSUMER_GIT_URI} /scratch/consumer
-cd /scratch/consumer/${CONSUMER_PATH}
-
-# checkout revision
-git checkout ${REVISION}
-
-# run the tests with docker-compose
-# note: old system reads CF manifest env vars and sets them all here
-docker-compose pull ${DOCKER_COMPOSE_SERVICE:-code}
-docker-compose run --no-deps \
-  --entrypoint "${CONSUMER_SCRIPT}" \
-  -e DEPENDENCY_NAME=${PROVIDER_NAME} \
-  -e ${PROVIDER_HOST_KEY}=${PROVIDER_HOST} \
-  -e CDC_CONSUMER_NAME=${CONSUMER_NAME} \
-  -e CDC_CONSUMER_VERSION=${REVISION} \
-  -e CDC_PROVIDER_NAME=${PROVIDER_NAME} \
-  -e CDC_PROVIDER_VERSION=${GIT_REVISION} \
-  %s \
-  %s \
-  ${DOCKER_COMPOSE_SERVICE:-code}
-`, envOption, volumeOption)
+	return fmt.Sprintf(`
+export ENV_OPTIONS="%s"
+/cdc.sh`, envOption)
 }
