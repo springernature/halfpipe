c2a7db5614a86080dd57c141e852f6b65fa68973
Simon Johansson <simon@simonjohansson.com>
2022-08-08 16:04:00 +0200

    Support artifacts

--- e2e/concourse/docker-oci-build/.halfpipe.io
+++ e2e/concourse/docker-oci-build/.halfpipe.io
@@ -12,6 +12,23 @@ triggers:
 tasks:
 - type: docker-push
   name: push to docker registry
-  username: rob
+  username: uSeRnAmE
   password: verysecret
   image: springerplatformengineering/halfpipe_fly:thisIsMy_Tag
+
+- type: run
+  name: date to file
+  docker:
+    image: alpine
+  script: \date > dateFile
+  save_artifacts:
+    - dateFile
+
+- type: docker-push
+  name: build-with-artifact-and-vars
+  username: uSeRnAmE
+  password: verysecret
+  image: eu.gcr.io/halfpipe-io/engineering-enablement/simon-test-simple
+  vars:
+    PASSED_IN: SIMON
+  restore_artifacts: true
--- renderers/concourse/docker_push.go
+++ renderers/concourse/docker_push.go
@@ -9,73 +9,16 @@ import (
 )
 
 func (c Concourse) dockerPushJob(task manifest.DockerPush, basePath string, ociBuild bool) atc.JobConfig {
-	resourceName := manifest.DockerTrigger{Image: task.Image}.GetTriggerName()
-	if task.RestoreArtifacts {
-		return dockerPushJobWithRestoreArtifacts(task, resourceName, basePath, ociBuild)
-	}
-	return dockerPushJobWithoutRestoreArtifacts(task, resourceName, basePath, ociBuild)
-}
-
-func dockerPushJobWithoutRestoreArtifacts(task manifest.DockerPush, resourceName string, basePath string, ociBuild bool) atc.JobConfig {
-	fullBasePath := path.Join(gitDir, basePath)
-
 	var steps []atc.Step
-	if ociBuild {
+	resourceName := manifest.DockerTrigger{Image: task.Image}.GetTriggerName()
 
-		params := atc.TaskEnv{
-			"CONTEXT": fullBasePath,
-		}
-
-		for k, v := range convertVars(task.Vars) {
-			params[fmt.Sprintf("BUILD_ARG_%s", k)] = fmt.Sprintf("%s", v)
-		}
-
-		buildStep := &atc.TaskStep{
-			Name:       "build",
-			Privileged: true,
-			Config: &atc.TaskConfig{
-				Platform: "linux",
-				ImageResource: &atc.ImageResource{
-					Type: "registry-image",
-					Source: atc.Source{
-						"repository": "concourse/oci-build-task",
-					},
-				},
-				Params: params,
-				Run: atc.TaskRunConfig{
-					Path: "build",
-				},
-				Inputs: []atc.TaskInputConfig{
-					{Name: gitDir},
-				},
-				Outputs: []atc.TaskOutputConfig{
-					{Name: "image"},
-				},
-			},
-		}
-
-		putStep := &atc.PutStep{
-			Name: resourceName,
-			Params: atc.Params{
-				"image":   "image/image.tar",
-				"version": "6.6.6",
-			},
-		}
-		steps = append(steps, stepWithAttemptsAndTimeout(buildStep, task.GetAttempts(), task.GetTimeout()))
-		steps = append(steps, stepWithAttemptsAndTimeout(putStep, task.GetAttempts(), task.GetTimeout()))
-	} else {
-		step := &atc.PutStep{
-			Name: resourceName,
-			Params: atc.Params{
-				"build":         path.Join(fullBasePath, task.BuildPath),
-				"dockerfile":    path.Join(fullBasePath, task.DockerfilePath),
-				"tag_as_latest": true,
-				"tag_file":      task.GetTagPath(fullBasePath),
-				"build_args":    convertVars(task.Vars),
-			},
-		}
-		steps = append(steps, stepWithAttemptsAndTimeout(step, task.GetAttempts(), task.GetTimeout()))
+	fullBasePath := path.Join(gitDir, basePath)
+	if task.RestoreArtifacts {
+		fullBasePath = path.Join(dockerBuildTmpDir, basePath)
 	}
+
+	steps = append(steps, restoreArtifacts(task)...)
+	steps = append(steps, buildAndPush(task, resourceName, ociBuild, fullBasePath, task.RestoreArtifacts)...)
 
 	return atc.JobConfig{
 		Name:         task.GetName(),
@@ -84,52 +27,103 @@ func dockerPushJobWithoutRestoreArtifacts(task manifest.DockerPush, resourceName
 	}
 }
 
-func dockerPushJobWithRestoreArtifacts(task manifest.DockerPush, resourceName string, basePath string, ociBuild bool) atc.JobConfig {
-	copyArtifact := &atc.TaskStep{
-		Name: "copying-git-repo-and-artifacts-to-a-temporary-build-dir",
+func restoreArtifacts(task manifest.DockerPush) []atc.Step {
+	if task.RestoreArtifacts {
+		copyArtifact := &atc.TaskStep{
+			Name: "copying-git-repo-and-artifacts-to-a-temporary-build-dir",
+			Config: &atc.TaskConfig{
+				Platform: "linux",
+				ImageResource: &atc.ImageResource{
+					Type: "docker-image",
+					Source: atc.Source{
+						"repository": "alpine",
+					},
+				},
+				Run: atc.TaskRunConfig{
+					Path: "/bin/sh",
+					Args: []string{"-c", strings.Join([]string{
+						fmt.Sprintf("cp -r %s/. %s", gitDir, dockerBuildTmpDir),
+						fmt.Sprintf("cp -r %s/. %s", artifactsInDir, dockerBuildTmpDir),
+					}, "\n")},
+				},
+				Inputs: []atc.TaskInputConfig{
+					{Name: gitDir},
+					{Name: artifactsName},
+				},
+				Outputs: []atc.TaskOutputConfig{
+					{Name: dockerBuildTmpDir},
+				},
+			},
+		}
+		return append([]atc.Step{}, stepWithAttemptsAndTimeout(copyArtifact, task.GetAttempts(), task.Timeout))
+	}
+	return []atc.Step{}
+}
+
+func buildAndPushOci(task manifest.DockerPush, resourceName string, fullBasePath string, restore bool) []atc.Step {
+	var steps []atc.Step
+
+	params := atc.TaskEnv{
+		"CONTEXT": path.Join(fullBasePath, task.BuildPath),
+	}
+
+	for k, v := range convertVars(task.Vars) {
+		params[fmt.Sprintf("BUILD_ARG_%s", k)] = fmt.Sprintf("%s", v)
+	}
+
+	buildStep := &atc.TaskStep{
+		Name:       "build",
+		Privileged: true,
 		Config: &atc.TaskConfig{
 			Platform: "linux",
 			ImageResource: &atc.ImageResource{
-				Type: "docker-image",
+				Type: "registry-image",
 				Source: atc.Source{
-					"repository": "alpine",
+					"repository": "concourse/oci-build-task",
 				},
 			},
+			Params: params,
 			Run: atc.TaskRunConfig{
-				Path: "/bin/sh",
-				Args: []string{"-c", strings.Join([]string{
-					fmt.Sprintf("cp -r %s/. %s", gitDir, dockerBuildTmpDir),
-					fmt.Sprintf("cp -r %s/. %s", artifactsInDir, dockerBuildTmpDir),
-				}, "\n")},
+				Path: "build",
 			},
 			Inputs: []atc.TaskInputConfig{
 				{Name: gitDir},
-				{Name: artifactsName},
 			},
 			Outputs: []atc.TaskOutputConfig{
-				{Name: dockerBuildTmpDir},
+				{Name: "image"},
 			},
 		},
 	}
+	if restore {
+		buildStep.Config.Inputs = append(buildStep.Config.Inputs, atc.TaskInputConfig{Name: dockerBuildTmpDir})
+	}
 
-	fullBasePath := path.Join(dockerBuildTmpDir, basePath)
-	put := &atc.PutStep{
+	putStep := &atc.PutStep{
 		Name: resourceName,
 		Params: atc.Params{
-			"build":         path.Join(dockerBuildTmpDir, basePath, task.BuildPath),
-			"dockerfile":    path.Join(dockerBuildTmpDir, basePath, task.DockerfilePath),
+			"image":   "image/image.tar",
+			"version": "6.6.6",
+		},
+	}
+	steps = append(steps, stepWithAttemptsAndTimeout(buildStep, task.GetAttempts(), task.GetTimeout()))
+	steps = append(steps, stepWithAttemptsAndTimeout(putStep, task.GetAttempts(), task.GetTimeout()))
+	return steps
+}
+
+func buildAndPush(task manifest.DockerPush, resourceName string, ociBuild bool, fullBasePath string, restore bool) []atc.Step {
+	if ociBuild {
+		return buildAndPushOci(task, resourceName, fullBasePath, restore)
+	}
+
+	step := &atc.PutStep{
+		Name: resourceName,
+		Params: atc.Params{
+			"build":         path.Join(fullBasePath, task.BuildPath),
+			"dockerfile":    path.Join(fullBasePath, task.DockerfilePath),
 			"tag_as_latest": true,
 			"tag_file":      task.GetTagPath(fullBasePath),
 			"build_args":    convertVars(task.Vars),
 		},
 	}
-
-	return atc.JobConfig{
-		Name:   task.GetName(),
-		Serial: true,
-		PlanSequence: []atc.Step{
-			stepWithAttemptsAndTimeout(copyArtifact, task.GetAttempts(), task.Timeout),
-			stepWithAttemptsAndTimeout(put, task.GetAttempts(), task.Timeout),
-		},
-	}
+	return append([]atc.Step{}, stepWithAttemptsAndTimeout(step, task.GetAttempts(), task.GetTimeout()))
 }
