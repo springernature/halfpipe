337cd28dce8e42bdfbd3bb0cb84b472affe61ff1
Rob Whitby <rob.whitby@springer.com>
2022-08-18 17:07:00 +0100

    use trivy to scan docker images in actions

--- renderers/actions/docker_push.go
+++ renderers/actions/docker_push.go
@@ -19,7 +19,6 @@ func (a *Actions) dockerPushSteps(task manifest.DockerPush) (steps Steps) {
 	}
 
 	steps = append(steps, buildImage(a, task, buildArgs))
-	steps = append(steps, gcloudAuth(task)...)
 	if task.ShouldScanDockerImage() {
 		steps = append(steps, scanImage(task))
 	}
@@ -63,42 +62,18 @@ func buildImage(a *Actions, task manifest.DockerPush, buildArgs Env) Step {
 	return step
 }
 
-func gcloudAuth(task manifest.DockerPush) []Step {
-	steps := []Step{}
-	steps = append(steps, Step{
-		Name: "gCloud Auth",
-		Uses: "google-github-actions/auth@v0",
-		With: With{
-			{"credentials_json", githubSecrets.GCRPrivateKey},
-		},
-	})
-	steps = append(steps, Step{
-		Name: "gCloud Setup",
-		Uses: "google-github-actions/setup-gcloud@v0",
-		With: With{
-			{"project_id", "halfpipe-io"},
-			{"install_components", "local-extract"},
-		},
-	})
-	return steps
-}
-
 func scanImage(task manifest.DockerPush) Step {
-	run := []string{}
-	run = append(run, fmt.Sprintf(`gcloud artifacts docker images scan %s:%s --location=europe --additional-package-types=GO,MAVEN --format='value(response.scan)' > /tmp/image-scan.txt`, task.Image, "${{ env.GIT_REVISION }}"))
-	run = append(run, `gcloud artifacts docker images list-vulnerabilities $(cat /tmp/image-scan.txt) --format='table(vulnerability.effectiveSeverity, vulnerability.cvssScore, noteName, vulnerability.packageIssue[0].affectedPackage, vulnerability.packageIssue[0].affectedVersion.name, vulnerability.packageIssue[0].fixedVersion.name)'`)
-	run = append(run, `gcloud artifacts docker images list-vulnerabilities $(cat /tmp/image-scan.txt) --format='value(vulnerability.effectiveSeverity)' > /tmp/severities.txt`)
-	run = append(run, `echo "Vulnerability Summary:" >> $GITHUB_STEP_SUMMARY`)
-	run = append(run, "echo '```' >> $GITHUB_STEP_SUMMARY")
-	run = append(run, `cat /tmp/severities.txt | grep -v '^$' | sort | uniq -c >> $GITHUB_STEP_SUMMARY`)
-	run = append(run, "echo '```' >> $GITHUB_STEP_SUMMARY")
-	run = append(run, `echo >> $GITHUB_STEP_SUMMARY`)
-	run = append(run, `if grep -Exq "$SEVERITY" /tmp/severities.txt; then echo 'Failed vulnerability check' && exit 1; fi`)
-
 	step := Step{
-		Name: "Scan image for vulnerabilities",
-		Run:  strings.Join(run, "\n"),
-		Env:  Env{"SEVERITY": task.SeverityList("|")},
+		Name: "Run Trivy vulnerability scanner",
+		Uses: "aquasecurity/trivy-action@0.7.0",
+		With: With{
+			{"image-ref", task.Image + ":${{ env.GIT_REVISION }}"},
+			{"format", "table"},
+			{"exit-code", 1},
+			{"ignore-unfixed", true},
+			{"vuln-type", "os,library"},
+			{"severity", task.SeverityList(",")},
+		},
 	}
 	return step
 }
