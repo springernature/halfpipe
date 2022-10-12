f6f2d037d26cef6e1226a92e4a764a001a7478b8
Rob Whitby <rob.whitby@springer.com>
2022-10-12 11:11:58 +0100

    add test for lint error .IsWarning()

--- linters/errors_test.go
+++ linters/errors_test.go
@@ -19,9 +19,16 @@ func Test_ErrorWrapping(t *testing.T) {
 }
 
 func Test_ErrorString(t *testing.T) {
-	baseErr := ErrInvalidField
+	baseErr := newError("invalid field")
 	err := baseErr.WithValue("script").WithValue("not found").WithFile("build.sh")
 
 	assert.EqualError(t, baseErr, "invalid field")
 	assert.EqualError(t, err, "invalid field: script: not found (build.sh)")
+}
+
+func Test_ErrorIsWarning(t *testing.T) {
+	baseErr := newError("base")
+	assert.False(t, baseErr.IsWarning())
+	assert.True(t, baseErr.AsWarning().IsWarning())
+	assert.True(t, baseErr.AsWarning().WithValue("v").WithFile("f").IsWarning())
 }
