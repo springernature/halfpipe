44295d533ef97eee997d42ba22e699dbe521476e
Rob Whitby <rob.whitby@springer.com>
2023-08-29 17:09:22 +0100

    add output summary when actions update-pipeline fails

--- renderers/actions/update.go
+++ renderers/actions/update.go
@@ -31,13 +31,20 @@ func (a *Actions) updateSteps(task manifest.Update, man manifest.Manifest) Steps
 		If:   "steps.sync.outputs.synced == 'false'",
 		Run: `git config user.name halfpipe-io
 git config user.email halfpipe-io@springernature.com
-git commit -am "[halfpipe] synced workflow $GITHUB_WORKFLOW with halfpipe manifest"
-git push
-echo 'Halfpipe successfully updated the workflow' >> $GITHUB_STEP_SUMMARY
-echo >> $GITHUB_STEP_SUMMARY
-echo 'This happened because the workflow was generated from a halfpipe manifest with the ` + "`update-pipeline`" + ` feature enabled. It keeps the workflow in sync with the halfpipe manifest.' >> $GITHUB_STEP_SUMMARY
-echo >> $GITHUB_STEP_SUMMARY
-echo '[Halfpipe Documentation](https://ee.public.springernature.app/rel-eng/halfpipe/features/#update_pipeline)' >> $GITHUB_STEP_SUMMARY
+if git commit -am "[halfpipe] synced workflow $GITHUB_WORKFLOW with halfpipe manifest" && git push; then
+  echo ':white_check_mark: Halfpipe successfully updated the workflow' >> $GITHUB_STEP_SUMMARY
+  echo >> $GITHUB_STEP_SUMMARY
+  echo 'This happened because the workflow was generated from a halfpipe manifest with the ` + "`update-pipeline`" + ` feature enabled. It keeps the workflow in sync with the halfpipe manifest.' >> $GITHUB_STEP_SUMMARY
+  echo >> $GITHUB_STEP_SUMMARY
+  echo '[Halfpipe Documentation](https://ee.public.springernature.app/rel-eng/halfpipe/features/#update_pipeline)' >> $GITHUB_STEP_SUMMARY
+else
+  echo ':x: Halfpipe failed to update the workflow' >> $GITHUB_STEP_SUMMARY
+  echo >> $GITHUB_STEP_SUMMARY
+  echo 'This may have happened because newer git commits have already been pushed. Check for newer pipeline runs or manually trigger the workflow.' >> $GITHUB_STEP_SUMMARY
+  echo >> $GITHUB_STEP_SUMMARY
+  echo '[Halfpipe Documentation](https://ee.public.springernature.app/rel-eng/halfpipe/features/#update_pipeline)' >> $GITHUB_STEP_SUMMARY
+  exit 1
+fi
 `,
 	}
 
