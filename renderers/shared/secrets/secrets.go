583490654652ac2535ec617a1b6425af89801341
Rob Whitby <rob.whitby@springer.com>
2023-09-29 13:39:06 +0100

    refactor secret rendering

---
+++ renderers/shared/secrets/secrets.go
@@ -0,0 +1,87 @@
+package secrets
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Secret models a Vault secret
+// MapPath is root-relative path e.g. "/myteam/myproject/mysecretmap"
+type Secret struct {
+	MapPath string
+	Key     string
+}
+
+// New returns a Secret from a string in the "halfpipe" format
+// "((map.key))" or "((/path/to/map key))"
+func New(s string, team string) *Secret {
+	if !isSecret(s) {
+		return nil
+	}
+
+	secretValue := strings.TrimSpace(s[2 : len(s)-2])
+
+	if isKeyValueSecret(secretValue) {
+		parts := strings.Split(secretValue, ".")
+		if isSharedSecret(parts[0]) {
+			team = "shared"
+		}
+		return &Secret{
+			MapPath: fmt.Sprintf("%s/%s", team, parts[0]),
+			Key:     parts[1],
+		}
+	}
+
+	if isAbsolutePathSecret(secretValue) {
+		parts := strings.Split(secretValue, " ")
+		mapPath := strings.TrimPrefix(parts[0], "/springernature/data/")
+		mapPath = strings.TrimPrefix(mapPath, "/springernature/")
+		return &Secret{
+			MapPath: mapPath,
+			Key:     parts[1],
+		}
+	}
+
+	return nil
+}
+
+func isSecret(s string) bool {
+	return strings.HasPrefix(s, "((") && strings.HasSuffix(s, "))")
+}
+
+func isAbsolutePathSecret(s string) bool {
+	return len(strings.Split(s, " ")) == 2
+}
+
+func isKeyValueSecret(s string) bool {
+	return len(strings.Split(s, ".")) == 2
+}
+
+func isSharedSecret(s string) bool {
+	return map[string]bool{
+		"PPG-gradle-version-reporter":         true,
+		"PPG-owasp-dependency-reporter":       true,
+		"artifactory":                         true,
+		"artifactory-support":                 true,
+		"artifactory_test":                    true,
+		"bla":                                 true,
+		"burpsuiteenterprise":                 true,
+		"content_hub-casper-credentials-live": true,
+		"content_hub-casper-credentials-qa":   true,
+		"contrastsecurity":                    true,
+		"eas-sigrid":                          true,
+		"ee-sso-route-service":                true,
+		"fastly":                              true,
+		"grafana":                             true,
+		"halfpipe-artifacts":                  true,
+		"halfpipe-docker-config":              true,
+		"halfpipe-gcr":                        true,
+		"halfpipe-github":                     true,
+		"halfpipe-ml-deploy":                  true,
+		"halfpipe-semver":                     true,
+		"halfpipe-slack":                      true,
+		"katee-tls-dev":                       true,
+		"katee-tls-prod":                      true,
+		"sentry-release-integration":          true,
+	}[s]
+}
--- renderers/shell/shell.go
+++ renderers/shell/shell.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/springernature/halfpipe"
 	"github.com/springernature/halfpipe/manifest"
+	"github.com/springernature/halfpipe/renderers/shared/secrets"
 	"strings"
 )
 
@@ -36,7 +37,7 @@ func renderRunCommand(task manifest.Run, team string) string {
 	}
 
 	for k, v := range task.Vars {
-		s = append(s, fmt.Sprintf("-e %s=%s", k, vaultLookup(v, team)))
+		s = append(s, fmt.Sprintf("-e %s=%s", k, convertSecret(v, team)))
 	}
 
 	s = append(s, task.Docker.Image, task.Script)
@@ -54,7 +55,7 @@ func renderDockerComposeCommand(task manifest.DockerCompose, team string) string
 	}
 
 	for k, v := range task.Vars {
-		s = append(s, fmt.Sprintf("-e %s=%s", k, vaultLookup(v, team)))
+		s = append(s, fmt.Sprintf("-e %s=%s", k, convertSecret(v, team)))
 	}
 
 	s = append(s, "--use-aliases", task.Service)
@@ -66,71 +67,10 @@ func renderDockerComposeCommand(task manifest.DockerCompose, team string) string
 	return strings.Join(s, " \\ \n  ")
 }
 
-func vaultLookup(s string, team string) string {
-	if !isSecret(s) {
+func convertSecret(s string, team string) string {
+	secret := secrets.New(s, team)
+	if secret == nil {
 		return s
 	}
-	s = strings.TrimSpace(s[2 : len(s)-2])
-
-	if isKeyValueSecret(s) {
-		parts := strings.Split(s, ".")
-		vaultFolder := team
-		if isShared(parts[0]) {
-			vaultFolder = "shared"
-		}
-		return fmt.Sprintf("$(vault kv get -field=%s /springernature/%s/%s)", parts[1], vaultFolder, parts[0])
-	}
-
-	if isAbsolutePathSecret(s) {
-		parts := strings.Split(s, " ")
-		return fmt.Sprintf("$(vault kv get -field=%s /springernature/%s/%s)", parts[1], team, parts[0])
-	}
-
-	return s
-}
-
-// **************************************************
-// all this copied from renderers/actions/secrets.go
-
-// check if a secret matches one of the shared secrets
-// vault kv list /springernature/shared
-func isShared(s string) bool {
-	return map[string]bool{
-		"PPG-gradle-version-reporter":         true,
-		"PPG-owasp-dependency-reporter":       true,
-		"artifactory":                         true,
-		"artifactory-support":                 true,
-		"artifactory_test":                    true,
-		"bla":                                 true,
-		"burpsuiteenterprise":                 true,
-		"content_hub-casper-credentials-live": true,
-		"content_hub-casper-credentials-qa":   true,
-		"contrastsecurity":                    true,
-		"eas-sigrid":                          true,
-		"ee-sso-route-service":                true,
-		"fastly":                              true,
-		"grafana":                             true,
-		"halfpipe-artifacts":                  true,
-		"halfpipe-docker-config":              true,
-		"halfpipe-gcr":                        true,
-		"halfpipe-github":                     true,
-		"halfpipe-ml-deploy":                  true,
-		"halfpipe-semver":                     true,
-		"halfpipe-slack":                      true,
-		"katee-tls-dev":                       true,
-		"katee-tls-prod":                      true,
-		"sentry-release-integration":          true,
-	}[s]
-}
-
-func isSecret(s string) bool {
-	return strings.HasPrefix(s, "((") && strings.HasSuffix(s, "))")
-}
-
-func isAbsolutePathSecret(s string) bool {
-	return len(strings.Split(s, " ")) == 2
-}
-
-func isKeyValueSecret(s string) bool {
-	return len(strings.Split(s, ".")) == 2
+	return fmt.Sprintf("$(vault kv get -field=%s /springernature/%s)", secret.Key, secret.MapPath)
 }
