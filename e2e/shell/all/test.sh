2aa5e8ead35e54f8b2eccbd27d306c33a9ba062b
Simon Johansson <simon@simonjohansson.com>
2024-04-29 10:56:23 +0200

    The substitution breaks on my mac. Simplify it

--- e2e/shell/all/test.sh
+++ e2e/shell/all/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for f in `find . -name '*_expected.txt'`; do
-  taskName="${f:2:(-13)}"
+for f in $(ls | grep _expected.txt ); do
+  taskName=$(echo $f | cut -d _ -f 1)
   echo "  task name: $taskName"
   ../../../halfpipe -q exec "$taskName" > "${f/expected/actual}"
   diff -w "$f" "${f/expected/actual}"
