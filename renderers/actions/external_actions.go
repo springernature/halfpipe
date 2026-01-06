d87efbd2879fb7f4cd07651205439f9b97bf639a
Simon Johansson <simon@simonjohansson.com>
2026-01-06 09:26:13 +0100

    New version of upload and download-artifact

--- renderers/actions/external_actions.go
+++ renderers/actions/external_actions.go
@@ -20,10 +20,10 @@ var ExternalActions = struct {
 	DeployKatee:        "springernature/ee-action-deploy-katee@v1",
 	DockerLogin:        "docker/login-action@9780b0c442fbb1117ed29e0efdff1e18412f7567", // v3
 	DockerPush:         "springernature/ee-action-docker-push@v1",
-	DownloadArtifact:   "actions/download-artifact@018cc2cf5baa6db3ef3c5f8a56943fffe632ef53",       // v6
+	DownloadArtifact:   "actions/download-artifact@37930b1c2abaa49bbe596cd826c3c89aef350131",       // v7
 	RepositoryDispatch: "peter-evans/repository-dispatch@28959ce8df70de7be546dd1250a005dd32156697", // v4
 	Slack:              "slackapi/slack-github-action@91efab103c0de0a537f72a35f6b8cda0ee76bf0a",    //v2
 	Teams:              "jdcargile/ms-teams-notification@28e5ca976c053d54e2b852f3f38da312f35a24fc", // v1
-	UploadArtifact:     "actions/upload-artifact@330a01c490aca151604b8cf639adc76d48f6c5d4",         // v5
+	UploadArtifact:     "actions/upload-artifact@b7c566a772e6b6bfb58ed0dc250532a479d7789f",         // v6
 	Vault:              "hashicorp/vault-action@4c06c5ccf5c0761b6029f56cfb1dcf5565918a3b",          // v3
 }
