2040c27e44af7bd403d1c5b1ffec620483c7ea4f
Rob Whitby <rob.whitby@springer.com>
2026-03-27 14:00:43 +0000

    update rendered actions

--- renderers/actions/external_actions.go
+++ renderers/actions/external_actions.go
@@ -18,12 +18,12 @@ var ExternalActions = struct {
 	Checkout:           "actions/checkout@de0fac2e4500dabe0009e67214ff5f5447ce83dd", // v6
 	DeployCF:           "springernature/ee-action-deploy-cf@v1",
 	DeployKatee:        "springernature/ee-action-deploy-katee@v1",
-	DockerLogin:        "docker/login-action@9780b0c442fbb1117ed29e0efdff1e18412f7567", // v3
+	DockerLogin:        "docker/login-action@b45d80f862d83dbcd57f89517bcf500b2ab88fb2", // v4.0.0
 	DockerPush:         "springernature/ee-action-docker-push@v1",
-	DownloadArtifact:   "actions/download-artifact@37930b1c2abaa49bbe596cd826c3c89aef350131",       // v7
+	DownloadArtifact:   "actions/download-artifact@3e5f45b2cfb9172054b4087a40e8e0b5a5461e7c",       // v8.0.1
 	RepositoryDispatch: "peter-evans/repository-dispatch@28959ce8df70de7be546dd1250a005dd32156697", // v4
-	Slack:              "slackapi/slack-github-action@91efab103c0de0a537f72a35f6b8cda0ee76bf0a",    //v2
+	Slack:              "slackapi/slack-github-action@af78098f536edbc4de71162a307590698245be95",    // v3.0.1
 	Teams:              "jdcargile/ms-teams-notification@28e5ca976c053d54e2b852f3f38da312f35a24fc", // v1
-	UploadArtifact:     "actions/upload-artifact@b7c566a772e6b6bfb58ed0dc250532a479d7789f",         // v6
+	UploadArtifact:     "actions/upload-artifact@bbbca2ddaa5d8feaa63e36b76fdaad77386f024f",         // v7.0.0
 	Vault:              "hashicorp/vault-action@4c06c5ccf5c0761b6029f56cfb1dcf5565918a3b",          // v3
 }
