6e214f878773e73ffd1e05734aa1e3aa46b5ab71
Rob Whitby <rob.whitby@springer.com>
2026-04-07 17:30:35 +0100

    add e2e test for when on a branch

---
+++ .e2e/branch/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export HALFPIPE_BRANCH="feature-a/branch-b"
+
+if [[ -f actions.expected.yml ]]; then
+  "${HALFPIPE}" -q -p actions -o actions.actual.yml
+  echo "  Actions"
+  diff --ignore-blank-lines actions.actual.yml actions.expected.yml&& echo -e "\e[A✓"
+fi
+
+if [[ -f concourse.expected.yml ]]; then
+  "${HALFPIPE}" -q -p concourse -o concourse.actual.yml
+  echo "  Concourse"
+  diff --ignore-blank-lines concourse.actual.yml concourse.expected.yml&& echo -e "\e[A✓"
+fi
