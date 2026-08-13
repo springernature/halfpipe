7db96e6aaa6038223a86635d06e24247e097afd1
Simon Johansson <simon@simonjohansson.com>
2026-08-13 09:11:18 +0200

    Fail halfpipe if opslevel.yml is missing or system isnt a APPL

--- linters/opslevel_test.go
+++ linters/opslevel_test.go
@@ -8,35 +8,32 @@ import (
 )
 
 func TestOpsLevelSystem(t *testing.T) {
-	t.Run("warning when opslevel.yml not found", func(t *testing.T) {
+	t.Run("error when opslevel.yml not found", func(t *testing.T) {
 		man := manifest.Manifest{}
 		result := opsLevelLinter{}.Lint(man)
-		assert.True(t, result.HasWarnings())
-		assert.False(t, result.HasErrors())
+		assert.True(t, result.HasErrors())
 		assertContainsError(t, result.Issues, ErrOpsLevelNotFound)
 	})
 
-	t.Run("warning when opslevel.yml is invalid", func(t *testing.T) {
+	t.Run("error when opslevel.yml is invalid", func(t *testing.T) {
 		man := manifest.Manifest{
 			OpsLevel: manifest.OpsLevel{RelativePath: "opslevel.yml", ParseError: "failed to parse opslevel.yml: yaml: did not find expected ',' or ']'"},
 		}
 		result := opsLevelLinter{}.Lint(man)
-		assert.True(t, result.HasWarnings())
-		assert.False(t, result.HasErrors())
+		assert.True(t, result.HasErrors())
 		assertContainsError(t, result.Issues, ErrOpsLevelInvalid)
 	})
 
-	t.Run("warning when system is empty", func(t *testing.T) {
+	t.Run("error when system is empty", func(t *testing.T) {
 		man := manifest.Manifest{
 			OpsLevel: manifest.OpsLevel{RelativePath: "opslevel.yml"},
 		}
 		result := opsLevelLinter{}.Lint(man)
-		assert.True(t, result.HasWarnings())
-		assert.False(t, result.HasErrors())
+		assert.True(t, result.HasErrors())
 		assertContainsError(t, result.Issues, ErrInvalidField.WithValue("component.system"))
 	})
 
-	t.Run("no warning when system matches pattern", func(t *testing.T) {
+	t.Run("no error when system matches pattern", func(t *testing.T) {
 		man := manifest.Manifest{
 			OpsLevel: manifest.OpsLevel{RelativePath: "opslevel.yml", System: "APPL-428"},
 		}
@@ -45,13 +42,12 @@ func TestOpsLevelSystem(t *testing.T) {
 		assert.False(t, result.HasErrors())
 	})
 
-	t.Run("warning when system does not match pattern", func(t *testing.T) {
+	t.Run("error when system does not match pattern", func(t *testing.T) {
 		man := manifest.Manifest{
 			OpsLevel: manifest.OpsLevel{RelativePath: "opslevel.yml", System: "oscar"},
 		}
 		result := opsLevelLinter{}.Lint(man)
-		assert.True(t, result.HasWarnings())
-		assert.False(t, result.HasErrors())
+		assert.True(t, result.HasErrors())
 		assertContainsError(t, result.Issues, ErrInvalidField.WithValue("component.system"))
 	})
 }
